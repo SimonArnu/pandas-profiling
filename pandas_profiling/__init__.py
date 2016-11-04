ff63aeeeff7798a4aa08ba326de04bf34ee72aef
SimonArnu <simon@arnu.de>
2016-11-04 13:45:38 +0100

    Update __init__.py

--- pandas_profiling/__init__.py
+++ pandas_profiling/__init__.py
@@ -1,3 +1,4 @@
+
 import codecs
 from .templates import template
 from .base import describe, to_html
@@ -10,14 +11,14 @@ class ProfileReport(object):
     html = ''
     file = None
 
-    def __init__(self, df, **kwargs):
+    def __init__(self, df, report_title='Overview', **kwargs):
 
         sample = kwargs.get('sample', df.head())
 
         description_set = describe(df, **kwargs)
 
         self.html = to_html(sample,
-                            description_set)
+                            description_set, report_title)
 
         self.description_set = description_set
 
@@ -41,14 +42,17 @@ class ProfileReport(object):
         if outputfile != NO_OUTPUTFILE:
             if outputfile == DEFAULT_OUTPUTFILE:
                 outputfile = 'profile_' + str(hash(self)) + ".html"
+
+            self.file = codecs.open(outputfile, 'w+b', encoding='utf8')
             # TODO: should be done in the template
-            with codecs.open(outputfile, 'w+b', encoding='utf8') as self.file:
-                self.file.write(templates.template('wrapper').render(content=self.html))
-
-
+            self.file.write(templates.template('wrapper').render(content=self.html))
+            self.file.close()
 
     def _repr_html_(self):
         return self.html
 
     def __str__(self):
         return "Output written to file " + str(self.file.name)
+
+
+
